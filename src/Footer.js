99913cccf2adec1f3126c8e54897fb1e2e16bac5
srzvan <sbingu.razvan@gmail.com>
2020-09-29 19:49:52 +0300

    <Header /> , <Main /> & <Footer /> scaffold

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made with
+        <span role="img" aria-labelledby="heart">
+          💖
+        </span>
+        by <strong>Răzvan Sbîngu</strong>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main(props) {
+  return <main className="main">{props.children}</main>;
+}
+
+export default Main;
