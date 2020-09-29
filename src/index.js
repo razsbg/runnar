bca3e57eed73d45b1f0678ebe9641004545153f5
srzvan <sbingu.razvan@gmail.com>
2020-09-29 19:50:57 +0300

    imported styles in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import css from './assets/normalize.min.css';
+import './scss/_main.scss';
 
 ReactDOM.render(
   <React.StrictMode>
