b57c446a477c507f628b69810598394b5098eb51
srzvan <sbingu.razvan@gmail.com>
2020-09-29 19:50:28 +0300

    app placeholder text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <Header />
+      <Main>
+        <h2>
+          Hello there
+          <span role="img" aria-labelledby="Sparkles">
+            ✨
+          </span>
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem Ipsum best not make any more threats to your website. It will be
+          met with fire and fury like the world has never seen. All of the words
+          in Lorem Ipsum have flirted with me - consciously or unconsciously.
+          That's to be expected. You have so many different things placeholder
+          text has to be able to do, and I don't believe Lorem Ipsum has the
+          stamina. Lorem Ipsum is the single greatest threat. We are not - we
+          are not keeping up with other websites. You have so many different
+          things placeholder text has to be able to do, and I don't believe
+          Lorem Ipsum has the stamina.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Main>
+      <Footer />
     </div>
   );
 }
