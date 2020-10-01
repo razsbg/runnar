7eaa2779078a5a2c4b83d96175ea1723a117624e
srzvan <sbingu.razvan@gmail.com>
2020-10-01 23:14:23 +0300

    moved <DnDProvider /> & its children into <Main />

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import RoutePlanner from './RoutePlanner';
-import Lap from './Lap';
-
-import { laps } from './constants';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Main>
-        <DndProvider backend={HTML5Backend}>
-          <div className="drag-sources">
-            <h2>Laps</h2>
-            {Object.keys(laps).map((lap) => (
-              <Lap key={laps[lap].name} {...laps[lap]} />
-            ))}
-          </div>
-          <RoutePlanner />
-        </DndProvider>
-      </Main>
+      <Main />
       <Footer />
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-function Main(props) {
-  return <main className="main">{props.children}</main>;
+import Lap from './Lap';
+import RoutePlanner from './RoutePlanner';
+
+import { laps } from './constants';
+
+function Main() {
+  return (
+    <main className="main">
+      <DndProvider backend={HTML5Backend}>
+        <div className="drag-sources">
+          <h2>Laps</h2>
+          {Object.keys(laps).map((lap) => (
+            <Lap key={laps[lap].name} {...laps[lap]} />
+          ))}
+        </div>
+        <RoutePlanner />
+      </DndProvider>
+    </main>
+  );
 }
 
 export default Main;
