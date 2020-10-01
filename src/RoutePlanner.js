970b9fec43910fbc53702517b6dee9408933bab8
srzvan <sbingu.razvan@gmail.com>
2020-10-01 21:26:30 +0300

    added useDrop hook & styles to <RoutePlanner />

---
+++ src/RoutePlanner.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
+
+import { DragItemTypes, laps } from './constants';
+
+import './scss/components/_route-planner.scss';
+
+function RoutePlanner(props) {
+  const [route, setRoute] = useState([]);
+
+  const [{ isOver }, drop] = useDrop({
+    accept: DragItemTypes.LAP,
+    drop: (item) => {
+      setRoute([...route, item.name]);
+    },
+    canDrop: (item) => {
+      if (isLapEligible(item)) {
+        return true;
+      }
+
+      return false;
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  });
+
+  function isLapEligible(lap) {
+    const currentLastLap = [...route].pop() || '';
+    let isEligible = true;
+
+    switch (lap.name) {
+      case 'small':
+      case 'medium':
+      case 'large':
+        if (currentLastLap.name === laps.xxlarge.name) {
+          isEligible = false;
+        }
+
+        break;
+      case 'xlarge':
+        if (currentLastLap.name === laps.medium.name) {
+          isEligible = false;
+        }
+
+        break;
+      case 'xxlarge':
+        if (
+          currentLastLap.name === laps.small.name ||
+          currentLastLap.name === laps.medium.name ||
+          currentLastLap.name === laps.large.name
+        ) {
+          isEligible = false;
+        }
+
+        break;
+      default:
+        break;
+    }
+
+    return isEligible;
+  }
+
+  return (
+    <div ref={drop} className="route-planner">
+      <h2>RoutePlanner</h2>
+      {route.map((lapName) => (
+        <div className="route-lap">
+          <h3>{lapName}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RoutePlanner;
