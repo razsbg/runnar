a0cbc8ad7c4cbd868485c6def0dff962d4341ae3
srzvan <sbingu.razvan@gmail.com>
2020-10-01 21:25:14 +0300

    constants.js, added React-DnD provider in <App />

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import RoutePlanner from './RoutePlanner';
+import Lap from './Lap';
+
+import { laps } from './constants';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Main>
-        <h2>
-          Hello there
-          <span role="img" aria-labelledby="Sparkles">
-            ✨
-          </span>
-        </h2>
-        <p>
-          Lorem Ipsum best not make any more threats to your website. It will be
-          met with fire and fury like the world has never seen. All of the words
-          in Lorem Ipsum have flirted with me - consciously or unconsciously.
-          That's to be expected. You have so many different things placeholder
-          text has to be able to do, and I don't believe Lorem Ipsum has the
-          stamina. Lorem Ipsum is the single greatest threat. We are not - we
-          are not keeping up with other websites. You have so many different
-          things placeholder text has to be able to do, and I don't believe
-          Lorem Ipsum has the stamina.
-        </p>
+        <DndProvider backend={HTML5Backend}>
+          <div className="drag-sources">
+            <h2>Laps</h2>
+            {Object.keys(laps).map((lap) => (
+              <Lap key={laps[lap].name} {...laps[lap]} />
+            ))}
+          </div>
+          <RoutePlanner />
+        </DndProvider>
       </Main>
       <Footer />
     </div>
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const DragItemTypes = {
+  LAP: 'lap',
+};
+
+export const laps = {
+  small: {
+    name: 'small',
+    sides: [170, 49, 180, 54],
+  },
+  medium: {
+    name: 'medium',
+    sides: [350, 74, 350, 59],
+  },
+  large: {
+    name: 'large',
+    sides: [520, 74, 530, 54],
+  },
+  xlarge: {
+    name: 'xlarge',
+    sides: [600, 50, 650, 85],
+  },
+  xxlarge: {
+    name: 'xxlarge',
+    sides: [1100, 74, 1100, 82],
+  },
+};
