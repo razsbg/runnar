fadd116314e8f549778f118209b96ff73746fdef
srzvan <sbingu.razvan@gmail.com>
2020-10-06 21:07:06 +0300

    refactor format distance from helpers, test suite for helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,8 @@
 export function formatDistanceInKms(distance) {
+  if (typeof distance != 'number' || Object.is(distance, NaN)) {
+    return NaN;
+  }
+
   const d = Number.parseFloat(distance);
 
   return (d / 1000).toFixed(2);
