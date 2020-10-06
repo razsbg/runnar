4eb80c13bfa3ebfed7a88bcd448f090e89f1aef0
srzvan <sbingu.razvan@gmail.com>
2020-10-06 19:27:42 +0300

    display lap length in <RouteLap />

--- src/components/RouteLap.js
+++ src/components/RouteLap.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { formatDistanceInKms } from '../helpers';
+
 import '../scss/components/_route-lap.scss';
 
 function RouteLap(props) {
   return (
     <div className="route-lap" onClick={() => props.removeLap(props.index)}>
-      <h3>{props.lapName}</h3>
+      <h4>{props.lapName}</h4>
+      <p>{formatDistanceInKms(props.length)} km</p>
     </div>
   );
 }
 
 RouteLap.propTypes = {
-  lapName: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
+  lapName: PropTypes.string.isRequired,
+  length: PropTypes.number.isRequired,
   removeLap: PropTypes.func.isRequired,
 };
 
