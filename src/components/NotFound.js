43b59c97595a8eade1eec04f33185fae230b8470
srzvan <sbingu.razvan@gmail.com>
2020-10-14 19:52:20 +0300

    <NotFound /> component

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../scss/components/_not-found.scss';
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h2>404</h2>
+      <h3>Sorry, this page doesn't exist.</h3>
+    </div>
+  );
+}
+
+export default NotFound;
