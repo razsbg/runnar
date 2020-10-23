2f8f2c10ed2a80e7295bec3ccbff701795c59f52
srzvan <sbingu.razvan@gmail.com>
2020-10-23 21:09:15 +0300

    changed lap names to shorther ones

--- src/constants.js
+++ src/constants.js
@@ -10,23 +10,23 @@ let lap = {
 
 export let laps = {
   small: Object.assign(Object.create(lap), {
-    name: 'small',
+    name: 's',
     sides: [170, 49, 180, 54],
   }),
   medium: Object.assign(Object.create(lap), {
-    name: 'medium',
+    name: 'm',
     sides: [350, 74, 350, 59],
   }),
   large: Object.assign(Object.create(lap), {
-    name: 'large',
+    name: 'l',
     sides: [520, 74, 530, 54],
   }),
   xlarge: Object.assign(Object.create(lap), {
-    name: 'xlarge',
+    name: 'xl',
     sides: [600, 50, 650, 85],
   }),
   xxlarge: Object.assign(Object.create(lap), {
-    name: 'xxlarge',
+    name: 'xxl',
     sides: [1100, 74, 1100, 82],
   }),
 };
